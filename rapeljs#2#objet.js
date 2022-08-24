a96055a79f986ae7cec5687070603ce86c7ac2f7
Ophelie22 <59674073+Ophelie22@users.noreply.github.com>
2022-08-24 22:49:02 +0200

    Create rapeljs#2#objet.js

---
+++ rapeljs#2#objet.js
@@ -0,0 +1,20 @@
+let utilisateurs = {
+  adresseA: {
+    nom: 'Musk',
+    prenom: 'Elon',
+    profession: 'Entrepreneur',
+    adresse: {
+      numero: '2',
+      rue: 'Etoile de la Fortune',
+      pays: 'France',
+    }
+ //ici on aura des objets ds des objets
+//presentation: function(){
+  },
+  adresseB: {
+    nom: 'olop',
+    prenom: 'Epon',
+    profession: 'Entrepreneur',
+   },
+}
+console.log(utilisateurs['adresseB']['nom']);
